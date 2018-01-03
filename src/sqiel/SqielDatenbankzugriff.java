bda821cf411d066def9ca57054b8f2e7e3fc7737
johanna.polzin <johanna.polzin@r117-14.ad.b-cw-hwos.logodidact.net>
2018-01-03 11:07:32 +0100

    Neue Datenbank Benutzerinfo nulnulnulnulnulnul...

--- src/sqiel/SqielDatenbankzugriff.java
+++ src/sqiel/SqielDatenbankzugriff.java
@@ -32,11 +32,41 @@ public class SqielDatenbankzugriff {
             System.out.println(e.getMessage());
         }
     }
+    
+    
+    
+     public static void kreierebenutzerinfo() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:BenutzerInfo.db";
+        /*
+        CREATE Benutzerinfo IF NOT EXISTS( 
+        bid Integer NOT NULL,
+        benutzer TEXT CONSTRAINT, 
+        passwort TEXT ,
+        Primary Key(bid)) ;
+        */
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS BenutzerInfo (\n"
+                + "	bid integer PRIMARY KEY,\n"
+                + "	benutzer text NOT NULL,\n"
+                + "	passwort text NOT NULL,\n"
+                + "	capacity real\n"
+                + ");";
+        
+        try (Connection conn = DriverManager.getConnection(url);
+                Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
  
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         createNewTable();
+        kreierebenutzerinfo();
     }
 }
